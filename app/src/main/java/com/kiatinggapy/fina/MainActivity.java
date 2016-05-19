6c2f540743ef0b81fd58b6a31482bbaf08803895
milawatihartono <milawatihartono22@gmail.com>
2016-05-19 23:32:47 +0700

    coding dikit, gatau bener apa ga :v:
    nambah buton, nambah gambar

--- app/src/main/java/com/kiatinggapy/fina/MainActivity.java
+++ app/src/main/java/com/kiatinggapy/fina/MainActivity.java
@@ -21,6 +21,9 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
---
+++ app/src/main/java/com/kiatinggapy/fina/SecondActivity.java
@@ -0,0 +1,13 @@
+package com.kiatinggapy.fina;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Mila on 19/05/2016.
+ */
+public class SecondActivity extends AppCompatActivity {
+
+
+
+
+}
